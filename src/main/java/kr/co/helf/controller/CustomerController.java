a00c43cd7e18f36d46481fb44bb1956ca8e95fbe
oooohri <oooorrrr0117@gmail.com>
2023-07-28 20:09:06 +0900

    로그인
    로그인폼(미)

--- src/main/java/kr/co/helf/controller/CustomerController.java
+++ src/main/java/kr/co/helf/controller/CustomerController.java
@@ -1,9 +1,17 @@
 package kr.co.helf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/cust")
 public class CustomerController {
 
+	// 로그인폼 화면 요청을 처리하는 요청핸들러 메소드
+	@GetMapping(value="/login")
+	public String form() {
+		return "customer/loginform";
+	}
 	
 }
