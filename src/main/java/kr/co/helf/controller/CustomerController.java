959a0aeff4b1278e53a86162cf612766d21ec57c
juno <ojo1128@nate.com>
2023-07-30 20:16:56 +0900

    loginform 작성
    템플릿 스타일시트 사용해야합니다.

--- src/main/java/kr/co/helf/controller/CustomerController.java
+++ src/main/java/kr/co/helf/controller/CustomerController.java
@@ -5,13 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/cust")
+@RequestMapping("/")
 public class CustomerController {
 
 	// 로그인폼 화면 요청을 처리하는 요청핸들러 메소드
-	@GetMapping(value="/login")
+	@GetMapping(value="/loginform")
 	public String form() {
-		return "customer/loginform";
+		return "loginform";
 	}
 	
 }
