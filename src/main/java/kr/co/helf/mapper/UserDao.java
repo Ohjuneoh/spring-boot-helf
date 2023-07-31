f8b8a09eff09aae813ec537450629e923644d689
juno <ojo1128@nate.com>
2023-07-31 17:27:55 +0900

    로그인 기능 추가
    Spring Security 적용 비밀번호 암호화

--- src/main/java/kr/co/helf/mapper/UserDao.java
+++ src/main/java/kr/co/helf/mapper/UserDao.java
@@ -1,5 +1,14 @@
 package kr.co.helf.mapper;
 
-public class UserDao {
+import org.apache.ibatis.annotations.Mapper;
+
+import kr.co.helf.vo.User;
+
+@Mapper
+public interface UserDao {
+	
+	User getUserById(String id);
+
+	void insertUser(User user);
 
 }
