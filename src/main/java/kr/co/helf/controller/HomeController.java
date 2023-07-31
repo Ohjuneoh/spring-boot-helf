e33044ba03a5e0dd2688ab616d529499cd94a346
juno <ojo1128@nate.com>
2023-07-31 12:41:57 +0900

    수정
    수정

--- src/main/java/kr/co/helf/controller/HomeController.java
+++ src/main/java/kr/co/helf/controller/HomeController.java
@@ -1,7 +1,6 @@
 package kr.co.helf.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -34,42 +33,4 @@ public class HomeController {
 		
 		return "home";	
 	}
-	
-	@GetMapping(value="/about")
-	public String about() {
-		
-		return "about";
-		
-	}
-	
-	@GetMapping(value="/blog")
-	public String blog() {
-		
-		return "blog";
-		
-	}
-	
-	@GetMapping(value="/detail")
-	public String detail() {
-		
-		return "detail";
-		
-	}
-	
-	@GetMapping(value="/feature")
-	public String feature() {
-		
-		return "feature";
-		
-	}
-	
-	@GetMapping(value="/lesson")
-	public String lesson() {
-		
-		return "lesson";
-		
-	}
-	
-	
-	
 }
