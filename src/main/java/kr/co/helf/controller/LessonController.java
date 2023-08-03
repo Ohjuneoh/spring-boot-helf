cca138c9ed2a5363d691e78654c348153cd5fc4a
oooohri <oooorrrr0117@gmail.com>
2023-08-03 15:17:06 +0900

    로그인 오류수정
    완료

--- src/main/java/kr/co/helf/controller/LessonController.java
+++ src/main/java/kr/co/helf/controller/LessonController.java
@@ -1,11 +1,13 @@
 package kr.co.helf.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 
 @Controller
 @RequestMapping("/lesson")
