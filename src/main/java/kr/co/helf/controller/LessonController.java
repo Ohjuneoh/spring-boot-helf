bce9f1d6430c6f1eca54daed30d61ff13cbfbf46
howlpang <bbaa6909@naver.com>
2023-08-03 12:42:48 +0900

    ex1222

--- src/main/java/kr/co/helf/controller/LessonController.java
+++ src/main/java/kr/co/helf/controller/LessonController.java
@@ -12,18 +12,15 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class LessonController {
-	
-	@GetMapping(value="registration")
+
+	@GetMapping(value = "registration")
 	public String loginForm() {
 		return "lesson/form";
 	}
-	
-	@GetMapping(value="list")
+
+	@GetMapping(value = "list")
 	public String lessonList() {
 		return "lesson/list";
 	}
 
-	public static void main(String[] args) {
-
-	}
 }
