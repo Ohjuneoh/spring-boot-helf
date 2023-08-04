6f63267e30da5689cec56a1b877aa667ab8c20ff
howlpang <bbaa6909@naver.com>
2023-08-04 11:18:34 +0900

    수업 관련 페이지 일괄 수정 중

--- src/main/java/kr/co/helf/controller/GroupLessonController.java
+++ src/main/java/kr/co/helf/controller/GroupLessonController.java
@@ -1,33 +1,33 @@
-//package kr.co.helf.controller;
-//
-//import kr.co.helf.form.GroupLessonForm;
-//import kr.co.helf.service.GroupLessonService;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//import lombok.RequiredArgsConstructor;
-//
-//@Controller
-//@RequestMapping("/grouplesson1")
-//@RequiredArgsConstructor
-//public class GroupLessonController {
-//
-//    private final GroupLessonService groupLessonService;
-//    @GetMapping(value = "registration")
-//    public String lessonForm() {
-//        return "grouplesson1/form";
-//    }
+package kr.co.helf.controller;
+
+import kr.co.helf.form.GroupLessonForm;
+import kr.co.helf.service.GroupLessonService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/grouplesson")
+@RequiredArgsConstructor
+public class GroupLessonController {
+
+    private final GroupLessonService groupLessonService;
+    @GetMapping("/registration")
+    public String lessonForm() {
+        return "grouplesson/trainerform";
+    }
+    @GetMapping(value = "list")
+    public String lessonList() {
+        return "grouplesson/trainerlist";
+    }
+
 //    @PostMapping("/registration")
 //    public String lesson(GroupLessonForm groupLessonForm){
 //        groupLessonService.insertLesson(groupLessonForm);
-//        return "redirect:/grouplesson1/list";
+//        return "redirect:/grouplesson/list";
 //    }
-//
-//    @GetMapping(value = "list")
-//    public String lessonList() {
-//        return "/grouplesson1/list";
-//    }
-//
-//}
+
+}
