dfcd205c03e39563934d6557928b07d86c704641
juno <ojo1128@nate.com>
2023-08-21 19:13:35 +0900

    (강사) 1:1 개인수업 개설기능 완료
    개인수업기능 구현완료, Personal_lesson 테이블 insert 작업완료

---
+++ src/main/java/kr/co/helf/vo/PersonalLesson.java
@@ -0,0 +1,23 @@
+package kr.co.helf.vo;
+
+import java.util.Date;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.Data;
+
+@Data
+@Alias("PersonalLesson")
+public class PersonalLesson {
+	
+	private int no;
+	private String name;
+	private Date date;
+	private String time;
+	private String content;
+	private String status;
+	private MyMembership myMembership;
+	private Trainer trainer;
+	private User user;
+
+}
--- src/main/java/kr/co/helf/vo/TrainerCareer.java
+++ src/main/java/kr/co/helf/vo/TrainerCareer.java
@@ -8,7 +8,6 @@ import lombok.Data;
 import lombok.ToString;
 
 @Data
-@ToString
 @Alias("TrainerCareer")
 
 public class TrainerCareer {
