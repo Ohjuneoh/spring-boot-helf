9e9e642a3ae1a605a75306122f4312807290d663
oooohri <oooorrrr0117@gmail.com>
2023-09-02 11:16:51 +0900

    1:1문의
    .

---
+++ src/main/java/kr/co/helf/vo/Inquires.java
@@ -0,0 +1,33 @@
+package kr.co.helf.vo;
+
+import java.util.Date;
+
+import org.apache.ibatis.type.Alias;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
+@ToString
+@Alias("Inquiry")
+public class Inquires {
+
+	private int no;
+	private String type;
+	private String title;
+	private Date createDate;
+	private Date updateDate;
+	private String isAnswer;
+	private String answer;
+	private User user;
+	private String secret;
+	private String filename;
+	private String content;
+}
